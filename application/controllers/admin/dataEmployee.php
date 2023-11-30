8db08f67ae0c7d74d3f61e20cac038ce7a607711
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-11-30 15:54:48 +0700

    add employee action

--- application/controllers/admin/dataEmployee.php
+++ application/controllers/admin/dataEmployee.php
@@ -13,6 +13,75 @@ class DataEmployee extends CI_Controller{
         $this->load->view('templates_admin/footer');
 
     }
+
+    public function addData() {
+        $dataEmployee['title'] = "Add Employee";
+        $dataEmployee['position'] = $this->payrollModel->get_data('position')->result();
+        $this->load->view('templates_admin/header', $dataEmployee);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/AddDataEmployee', $dataEmployee);
+        $this->load->view('templates_admin/footer');
+    }
+
+    public function addDataAction() {
+        $this->_rules();
+
+        if ($this->form_validation->run() == FALSE) {
+            $this->addData();
+        } else {
+            $nik = $this->input->post('nik');
+            $name_employee = $this->input->post('name_employee');
+            $gender = $this->input->post('gender');
+            $position = $this->input->post('position');
+            $date_join = $this->input->post('date_join');
+            $status = $this->input->post('status');
+            $photo = $_FILES ['photo'].['name'] ;
+
+            if ($photo=''){
+            }else{
+                $config ['upload_path'] = './assets/photo';
+                $config ['allowed_types'] = 'jpg|jpeg|png|tiff';
+                $this->load->library ('upload',$config);
+                if (!$this->upload->do_upload('photo')){
+                    echo "Upload Photo Failed!";
+                } else {
+                    $photo = $this->upload->data('file_name');
+                }
+            }
+
+             $data = array(
+            'nik'            => $nik,
+            'name_employee'  => $name_employee,
+            'gender'         => $gender,
+            'position'       => $position,
+            'date_join'      => $date_join,
+            'status'         => $status,
+            'photo'          => $photo,
+            );
+
+            $this->payrollModel->insert_data($data, 'employees');
+            $this->session->set_flashdata('message', '<div class="alert alert-success alert-dismissible fade show" role="alert">
+                <strong>Add Data Success!</strong>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+            </button>
+            </div>');
+            redirect('admin/dataEmployee');
+            }
 }
 
+public function updateData($id) {
+    $where = array('id_employee' => $id);
+    $dataEmployee['title'] = 'Update';
+}
+
+public function _rules() {
+    $this->form_validation->set_rules('nik', 'nik', 'required');
+    $this->form_validation->set_rules('name_employee', 'name employee', 'required');
+    $this->form_validation->set_rules('gender', 'gender', 'required');
+    $this->form_validation->set_rules('date_join', 'Date Join', 'required');
+    $this->form_validation->set_rules('position', 'Position', 'required');
+    $this->form_validation->set_rules('status', 'Status', 'required');
+}
+}
 ?>
