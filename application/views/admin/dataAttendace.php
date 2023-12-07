734b3ccbec042741ae28bda75cef97b76a0ba5f2
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-07 14:31:40 +0700

    data attendace

--- application/views/admin/dataAttendace.php
+++ application/views/admin/dataAttendace.php
@@ -31,20 +31,13 @@
         
         <div class="form-group mb-2">
         <label for="staticEmail2">Year: </label>
-        <select class="form-control" name="month">
-            <option value="">--Choose Month--</option>
-            <option value="01">January</option>
-            <option value="02">February</option>
-            <option value="03">March</option>
-            <option value="04">April</option>
-            <option value="05">May</option>
-            <option value="06">June</option>
-            <option value="07">July</option>
-            <option value="08">August</option>
-            <option value="09">September</option>
-            <option value="10">October</option>
-            <option value="11">November</option>
-            <option value="12">December</option>
+        <select class="form-control" name="year">
+            <option value="">--Choose Year--</option>
+            <?php $year = date('Y');
+            for ($i=2020;$i<$year+5;$i++) {
+            ?>
+            <option value="<?php echo $i ?>"><?php echo $i ?></option>
+        <?php } ?>
         </select>
         </div>
     <button type="submit" class="btn btn-primary mb-2">Confirm identity</button>
