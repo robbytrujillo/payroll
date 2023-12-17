6609af5dba7582c8826d8bbc78b7829572dd2f54
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-18 05:56:50 +0700

    salary slip

--- application/controllers/admin/salarySlip.php
+++ application/controllers/admin/salarySlip.php
@@ -5,7 +5,10 @@ class SalarySlip extends CI_Controller {
     public function index() {
         $dataSalarySlip['title'] = "Employee Salary Slip";
         $dataSalarySlip['employee'] = $this->payrollModel->get_data('employees')->result();
-        
+        $this->load->view('templates_admin/header', $dataSalarySlip);
+        $this->load->view('templates_admin/sidebar');
+        $this->load->view('admin/salarySlip');
+        $this->load->view('templates_admin/footer');
     }
 }
 
