83036d6b8d31ce1113f96a312195da49f4f488ba
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-19 13:10:25 +0700

    filter salary slip

--- application/controllers/admin/salarySlip.php
+++ application/controllers/admin/salarySlip.php
@@ -13,6 +13,18 @@ class SalarySlip extends CI_Controller {
 
     public function printSalarySlip() {
         $dataSalarySlip['title'] = "Print Salary Slip";
+        $name = $this->input->post('name_employee');
+        $month = $this->input->post('month');
+        $year = $this->input->post('year');
+        $monthyear = $month.$year;
+        
+        $dataSalarySlip['print_slip'] = $this->db->query("SELECT employees.nik, employees.name_employee, 
+                position.name_position, position.basic_salary, position.transport_allowance, position.meal_allowance,
+                attendaces.alpha FROM employees INNER JOIN attendaces ON attendaces.nik = employees.nik
+                INNER JOIN position ON position.name_position = employees.position WHERE attendaces.month = '$monthyear' 
+                AND attendaces.name_employee = '$name'")->result();
+                // var_dump($slip);
+                // die();
         $this->load->view('templates_admin/header', $dataSalarySlip);
         $this->load->view('admin/printSalarySlip', $dataSalarySlip);
     }
---
+++ application/views/admin/printSalarySlip.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php $title ?></title>
+    <style type="text/css">
+        body {
+            font-size: Arial;
+            color: black;
+        }
+    </style>
+</head>
+<body>
+    <center>
+        <h1>IHBS</h1>
+        <h2>Employee Salary Slip</h2>
+        <hr style="width: 50%; border-width: 5px; color: black;">
+    </center>
+</body>
+</html>
