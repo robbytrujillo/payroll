effc4bba7736edae680cd82a56739f04d505164a
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-21 06:52:40 +0700

    password editing

--- application/controllers/passwordEditing.php
+++ application/controllers/passwordEditing.php
@@ -6,7 +6,7 @@ class passwordEditing extends CI_Controller {
         $dataPasswordEditing['title'] = "Password Editing";
         $this->load->view('templates_admin/header', $dataPasswordEditing);
         $this->load->view('templates_admin/sidebar');
-        $this->load->view('admin/dataPosition', $dataPasswordEditing);
+        $this->load->view('passwordEditing', $dataPasswordEditing);
         $this->load->view('templates_admin/footer');
     }
 }
