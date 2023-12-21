d0eb5f190bb00ef6b66c747bb62ba6a7f21dab27
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-21 10:47:57 +0700

    password editing

--- application/controllers/passwordEditing.php
+++ application/controllers/passwordEditing.php
@@ -9,6 +9,19 @@ class passwordEditing extends CI_Controller {
         $this->load->view('passwordEditing', $dataPasswordEditing);
         $this->load->view('templates_admin/footer');
     }
+
+    public function passwordEditingAction() {
+        $newPassword = $this->input->post('newPassword');
+        $confirmPassword = $this->input->post('confirmPassword');
+
+        $this->form_validation->set_rules('newPassword', 'New Password', 'required|matches[confirmPassword]');
+        $this->form_validation->set_rules('confirmPassword', 'Confirm Password', 'required');
+
+        if ($this->form_validation->run() == FALSE) {
+            
+        }
+
+    }
 }
 
 ?>
