29c8ae8b369479f241b77503bbf50f1b80560bd7
Robby Ilhamkusuma <robbyikusuma88@gmail.com>
2023-12-23 06:02:50 +0700

    data salary employee

---
+++ application/controllers/employee/dataSalary.php
@@ -0,0 +1,29 @@
+<?php
+
+class DataSalary extends CI_Controller{
+
+    public function __construct() {
+        parent::__construct();
+
+        if ($this->session->userdata('access_right') != '2') {
+            $this->session->set_flashdata('message', '<div class="alert alert-danger alert-dismissible fade show" role="alert">
+            <strong>you are not Login yet!</strong>
+                	<button type="button" class="close" data-dismiss="alert" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>');
+				redirect('Welcome');
+        }
+    }
+    public function index(){
+        $data['title'] = "Data Salary";
+        $id = $this->session->userdata('id_employee');
+        $data['employee'] = $this->db->query("SELECT * FROM employees WHERE id_employee = $id ")->result();
+        $this->load->view('templates_employee/header', $data);
+        $this->load->view('templates_employee/sidebar');
+        $this->load->view('employee/dataSalary', $data);
+        $this->load->view('templates_employee/footer');
+    }
+}
+
+?>
